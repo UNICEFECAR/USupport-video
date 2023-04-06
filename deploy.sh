baf2372f1e4cc78702fde01b44466f6666837831
Ivan Ivanov <ivan.h.ivanov99@gmail.com>
2023-04-06 01:27:12 +0100

    add prod config setup

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,47 @@
-REDEPLOY=$1
+ENV=$1
+REDEPLOY=$2
+
+if [ "$ENV" != 'staging' ] && [ "$ENV" != 'prod' ]
+then    
+    echo "Please select deployment environment: staging | prod"
+fi
 
 if [ "$REDEPLOY" = 'deploy' ] || [ "$REDEPLOY" = 'redeploy' ]
 then
-    # Build docker image of the service locally
-    docker build -t video:latest .
+    IMAGE_TAG=beta
+    if [ "$ENV" = 'staging' ]
+    then    
+        IMAGE_TAG=beta
+    elif [ "$ENV" = 'prod' ]
+    then 
+        IMAGE_TAG=latest
+    fi
 
-    docker tag video:latest 482053628475.dkr.ecr.eu-central-1.amazonaws.com/usupport-video-api
+    # Build docker image of the service locally
+    docker build -t video:$IMAGE_TAG .
+    docker tag video:$IMAGE_TAG 482053628475.dkr.ecr.eu-central-1.amazonaws.com/usupport-video-api:$IMAGE_TAG
 
     # Push image to 
-    docker push 482053628475.dkr.ecr.eu-central-1.amazonaws.com/usupport-video-api
+    docker push 482053628475.dkr.ecr.eu-central-1.amazonaws.com/usupport-video-api:$IMAGE_TAG
+
+    cd kube-config
+    
+    kubectl apply -f config.yaml
+
+    cd $ENV
 
     if [ "$REDEPLOY" = 'deploy' ]
     then
         # Update Kuberenetes Cluster applications for this API service
-        kubectl apply -f config.yaml -f secrets.yaml -f deployment.yaml -f service.yaml
+        kubectl apply -f secrets.yaml -f deployment.yaml
     elif [ "$REDEPLOY" = 'redeploy' ]
     then 
-        kubectl apply -f config.yaml -f secrets.yaml -f service.yaml
+        kubectl apply -f secrets.yaml
         kubectl rollout restart deployment video
     fi
+
+    cd ..
+    kubectl apply -f service.yaml
 
 else
     echo "Please select either to deploy or redeploy k8s pod"
